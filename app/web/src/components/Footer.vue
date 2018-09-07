837fe165afbb08b74639f7eec42c44c605c3e666
Siva Krishna <30602781+siva2krish@users.noreply.github.com>
2018-09-07 06:12:35 -0400

    updated footer

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-       Global Blackbelt Team
+       Siva Global Blackbelt Team
       </div>
       <div class="col-lg-6">
       </div>
