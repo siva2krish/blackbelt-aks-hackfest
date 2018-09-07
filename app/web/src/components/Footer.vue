8ec1ba05e271ca278f74d4810fd35682a36c795a
Siva Krishna <30602781+siva2krish@users.noreply.github.com>
2018-09-07 07:05:18 -0400

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-       elt Team
+       Azure siva black Team
       </div>
       <div class="col-lg-6">
       </div>
